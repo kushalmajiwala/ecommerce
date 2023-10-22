ed39964c020f4ced6965e68c5ee78ba37b00c3dd
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-10-22 23:15:20 +0530

    responsiveness changed

--- src/admin_comp/AdminLogin.jsx
+++ src/admin_comp/AdminLogin.jsx
@@ -43,28 +43,30 @@ const AdminLogin = () => {
     return (
         <>
             <div className='flex pt-24 justify-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen w-screen'>
-                <div className='w-5/6 sm:w-5/12 md:w-3/12 2xl:pt-12 opacity-100 h-4/5 bg-cyan-400 rounded-2xl shadow-2xl'>
-                    <div className='flex justify-center pt-4'>
-                        <img src="https://ngaxtqtjphtkyssalygr.supabase.co/storage/v1/object/public/images/unique_store_logo.png" alt="no-image" className='rounded-full w-24 y-24 2xl:w-32 2xl:y-32' />
-                    </div>
-                    <div className='flex justify-center pt-3'>
-                        <p className='text-3xl font-medium'>ADMIN LOGIN</p>
-                    </div>
-                    <div>
-                        <div className='flex justify-center mt-3 2xl:mt-5'>
-                            <span className="p-input-icon-left">
-                                <i className="pi pi-user" />
-                                <InputText placeholder="Enter Username" value={uname} onChange={(e) => setUname(e.target.value)} />
-                            </span>
+                <div className='flex justify-center items-center w-5/6 sm:w-5/12 md:w-3/12 opacity-100 h-4/5 bg-cyan-400 rounded-2xl shadow-2xl'>
+                    <div className='w-full'>
+                        <div className='flex justify-center'>
+                            <img src="https://ngaxtqtjphtkyssalygr.supabase.co/storage/v1/object/public/images/unique_store_logo.png" alt="no-image" className='rounded-full w-24 y-24 2xl:w-32 2xl:y-32' />
                         </div>
-                        <div className='flex justify-center mt-3 2xl:mt-5'>
-                            <span className="p-input-icon-left">
-                                <i className="pi pi-lock" />
-                                <InputText type="password" placeholder="Enter Password" value={upass} onChange={(e) => setUpass(e.target.value)} />
-                            </span>
+                        <div className='flex justify-center'>
+                            <p className='text-3xl font-medium pt-3'>ADMIN LOGIN</p>
                         </div>
-                        <div className='flex justify-center pt-4'>
-                            <button className='w-4/6 2xl:w-3/6 2xl:mt-5 rounded-2xl bg-blue-600 hover:bg-blue-700 py-2 text-white text-lg' onClick={performLogin}>LOGIN</button>
+                        <div>
+                            <div className='flex justify-center mt-3'>
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-user" />
+                                    <InputText placeholder="Enter Username" value={uname} onChange={(e) => setUname(e.target.value)} />
+                                </span>
+                            </div>
+                            <div className='flex justify-center mt-3'>
+                                <span className="p-input-icon-left">
+                                    <i className="pi pi-lock" />
+                                    <InputText type="password" placeholder="Enter Password" value={upass} onChange={(e) => setUpass(e.target.value)} />
+                                </span>
+                            </div>
+                            <div className='flex justify-center pt-4'>
+                                <button className='w-4/6 2xl:w-3/6 2xl:mt-5 rounded-2xl bg-blue-600 hover:bg-blue-700 py-2 text-white text-lg' onClick={performLogin}>LOGIN</button>
+                            </div>
                         </div>
                     </div>
                 </div>
