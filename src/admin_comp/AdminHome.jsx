4ab4cf84175f8fac6a541fd765d25a0f46407b9d
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-10-24 21:51:30 +0530

    sidebar completed

--- src/admin_comp/AdminHome.jsx
+++ src/admin_comp/AdminHome.jsx
@@ -12,13 +12,8 @@ const AdminHome = () => {
     navigate("/admin");
   }
 
-  const dropdown = () => {
-    document.querySelector("#submenu").classList.toggle("hidden");
-    document.querySelector("#arrow").classList.toggle("rotate-0");
-  }
-
   const openSidebar = () => {
-    document.querySelector(".sidebar").classList.toggle("hidden");
+    document.querySelector(".sidebar").classList.toggle("transformed-state");
   }
 
   useEffect(() => {
@@ -46,40 +41,43 @@ const AdminHome = () => {
           </div>
         </div>
       </div> */}
+      
 
-      <span
-        class="absolute text-white text-4xl top-5 left-4 cursor-pointer"
-        onClick={openSidebar}
-      >
-        <i class="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
-      </span>
-      <div
-        class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
-      >
-        <div class="text-gray-100 text-xl">
-          <div class="p-2.5 mt-1 flex items-center">
-            <img src='https://ngaxtqtjphtkyssalygr.supabase.co/storage/v1/object/public/images/unique_store_logo.png' alt="no-image" className='w-20 y-20 rounded-full' />
-            <h1 class="font-bold text-gray-200 text-[15px] ml-3">ADMIN</h1>
-            <i
-              class="bi bi-x-lg text-red-500 cursor-pointer ml-28 lg:hidden"
-              onClick={openSidebar}
-            ></i>
+      <div className=' bg-gray-300 w-screen h-screen'>
+        <div>
+          <span
+            className="absolute text-white text-4xl top-5 left-4 cursor-pointer"
+            onClick={openSidebar}
+          >
+            <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
+          </span>
+          <div
+            className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900"
+          >
+            <div className="text-gray-100 text-xl">
+              <div className="p-2.5 mt-1 flex items-center">
+                <img src='https://ngaxtqtjphtkyssalygr.supabase.co/storage/v1/object/public/images/unique_store_logo.png' alt="no-image" className='w-20 y-20 rounded-full' />
+                <h1 className="font-bold text-gray-200 text-[15px] ml-3">ADMIN</h1>
+                <i
+                  className="bi bi-x-lg text-red-500 cursor-pointer ml-28"
+                  onClick={openSidebar}
+                ></i>
+              </div>
+              <div class="my-2 bg-gray-600 h-[1px]"></div>
+            </div>
+            {/* <div class="my-4 bg-gray-600 h-[1px]"></div> */}
+            <div
+              className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+              onClick={logout}>
+              <i className="bi bi-box-arrow-in-right text-2xl"></i>
+              <span className=" text-lg  ml-4 text-gray-200 font-bold">LOGOUT</span>
+            </div>
           </div>
-          <div class="my-2 bg-gray-600 h-[1px]"></div>
         </div>
-
-       
-        {/* <div class="my-4 bg-gray-600 h-[1px]"></div> */}
-       
-       
-        <div
-          class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
-          onClick={logout}>
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span class="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+        <div className='w-full h-20 bg-white shadow-sm'>
+          
         </div>
       </div>
-
     </>
   )
 }
