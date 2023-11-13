a1a56843b2eccaebd0d573b8c911bb36c1dcdb79
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-11-13 19:29:58 +0530

    seach bar added

--- src/admin_comp/AdminHome.jsx
+++ src/admin_comp/AdminHome.jsx
@@ -283,7 +283,45 @@ const AdminHome = () => {
           </div>
         </div>
         <div className={`w-full h-[90px] ${navColor} shadow-sm`}>
-          <div className=''>
+          <div className='md:flex md:justify-between'>
+            <div class="mb-3 ml-[45vh] hidden md:block">
+              <div class="relative mt-[23px] ml-3 flex w-[25vw] flex-wrap items-stretch">
+               {
+                mode === "light" ? 
+                <input
+                type="search"
+                className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-black outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon1" />
+                :
+                <input
+                type="search"
+                className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-white outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon1" />
+               }
+
+                <button
+                  class="relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg"
+                  type="button"
+                  id="button-addon1"
+                  data-te-ripple-init
+                  data-te-ripple-color="light">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    class="h-5 w-5">
+                    <path
+                      fill-rule="evenodd"
+                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                      clip-rule="evenodd" />
+                  </svg>
+                </button>
+              </div>
+            </div>
             <div className='flex justify-end h-[90px] items-center pr-5'>
               <div className='hidden md:flex items-center'>
                 <button className='ml-5 bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white font-medium shadow-md rounded-xl mr-5 edit-tooltip' data-pr-tooltip="EDIT PASSWORD" data-pr-position="bottom" onClick={redirectEditPasswordBig}><i className="bi bi-pencil-square"></i></button>
